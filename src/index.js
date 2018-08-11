966ea3cf9578f8550ff601df1f2c2e82f9c660ac
DevinPierce <devinmpierce@gmail.com>
2018-08-11 11:05:55 -0400

    Done.

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ import ReactDOM from 'react-dom';
 import SimpleComponent from './components/SimpleComponent';
 import SimplerComponent from './components/SimplerComponent';
 
+function goodClicking(){
+  alert("Good clicking, my friend")
+}
+
 ReactDOM.render(
   <div>
     <SimpleComponent />
-    <SimplerComponent handleClick={undefined} />
+    <SimplerComponent handleClick={goodClicking} />
   </div>,
   document.getElementById('root')
 );
